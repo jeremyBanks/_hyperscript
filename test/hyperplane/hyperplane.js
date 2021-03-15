4803031450c634792199fc76b7a85af10bde227a
carson <carson@leaddyno.com>
2021-03-15 11:05:36 -0600

    hyperplane initial thoughts

--- test/hyperplane/hyperplane.js
+++ test/hyperplane/hyperplane.js
@@ -14,6 +14,15 @@ describe("/// hyperplane", function() {
         div.innerHTML.should.equal("42");
     })
 
+    it("speed test", function(){
+        console.time("hyperplane")
+        var div = make("<div _='on click 1 set /foo to 42 on click 2 put /foo into me'></div>");
+        for (var i = 0; i < 10000; i++) {
+            div.click();
+        }
+        console.timeEnd("hyperplane");
+    })
+
     it("basic persistent hyperplane functionality", function(){
         localStorage.removeItem('hyperplane')
         var div = make("<div _='on click 1 set ///foo to 42 on click 2 put ///foo into me'></div>");
